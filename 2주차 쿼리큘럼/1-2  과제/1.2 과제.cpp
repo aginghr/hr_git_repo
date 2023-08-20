c53c440f0a6b00ddd5f1b085d8c4283c12f16d76
aginghr <hrlee@seceng.co.kr>
2023-08-21 01:32:49 +0900

    2주차 쿼리큘럼

--- 2주차 쿼리큘럼/1-2  과제/1.2 과제.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void)
-{
-	int a;
-	cout << a << endl;
-	return 0;
-}
--- 2주차 쿼리큘럼/1-2  과제/과제 1.1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void)
-{
-	string a = "H", b = "e", c = "l", d = "l", e = "o", f = "W", g = "o", h = "r", i = "l", j = "d";
-	printf("%s%s%s%s%s %s%s%s%s%s", a, b, c, d, e, f, g, h, i, j);
-	string word = a + b + c + d + e + " " + f + g + h + i + j;
-	printf("%s", word);
-}
